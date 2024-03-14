b4498fb3a053684ef536b1e4c1a9237712b35278
Oğuzhan Akçay <101995377+Oguzhan-Akcay@users.noreply.github.com>
2024-03-14 03:23:32 +0300

    Add files via upload

--- src/coponents/hero/Hero.jsx
+++ src/coponents/hero/Hero.jsx
@@ -20,13 +20,15 @@ function Hero() {
                 </h1>
                 <p className={`left-section-info ${darkMode === "true" ? "thema-left-section-info" : ""}`}>{content.description}</p>
                 <div className={`button-container-hero ${darkMode === "true" ? "thema-button-container-hero" : ""}`}>
-                    <button className={`hire-button ${darkMode === "true" ? "thema-hire-button" : ""}`}>{content.headerData.navigationLinks[2]}</button>
-                    <button className={`github-button ${darkMode === "true" ? "thema-github-button" : ""}`}> <img src={GithubLogo} alt="Github Logo" />Github</button>
-                    <button className={`linkedIn-button ${darkMode === "true" ? "thema-linkedIn-button" : ""}`}> <img src={LinkedInLogo} alt="Linkedin Logo" />Linkedin</button>
+                    <button className={`hire-button ${darkMode === "true" ? "thema-hire-button" : ""}`}>{content.headerData.navigationLinks[2]}</button>    
+                    <a href='https://github.com/Oguzhan-Akcay'><button className={`github-button ${darkMode === "true" ? "thema-github-button" : ""}`}> <img src={GithubLogo} alt="Github Logo" />         
+                    Github</button></a>
+                    <a href='https://www.linkedin.com/in/oguzhankcay/'><button className={`linkedIn-button ${darkMode === "true" ? "thema-linkedIn-button" : ""}`}> <img src={LinkedInLogo} alt="Linkedin Logo" />Linkedin</button>
+</a>
                 </div>
             </div>
             <div className={` right-section ${darkMode === "true" ? "thema-right-section" : ""}`}>
-                <img className={`hero-photo ${darkMode === "true" ? "thema-hero-photo" : ""}`} src="public\Oguz.jpg" alt="Oğuzhan Akçay" />
+                <img className={`hero-photo ${darkMode === "true" ? "thema-hero-photo" : ""}`} src="/Oguz.png" alt="Oğuzhan Akçay" />
             </div>
         </div>
     );
